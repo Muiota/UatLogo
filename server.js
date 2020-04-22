a9b11d94483c0100aaadba4f3e5a27c35c3a949c
Muiota <PhilPro2006@yandex.ru>
2020-04-22 17:07:30 +0300

    dlease enter the commit message for your changes. Lines starting

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-
+//https://raw.githubusercontent.com/afonsopacifer/cdn/master/earthmap1k.jpg
+//https://raw.githubusercontent.com/afonsopacifer/cdn/master/jupitermap.jpg
 connect()
     .use(serveStatic(__dirname))
     .listen(8081, () => console.log('Server running on 8081...'));
