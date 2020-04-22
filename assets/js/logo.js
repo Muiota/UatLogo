658f683ff836cbbc06bb050dba818dd50f2c8acf
Muiota <PhilPro2006@yandex.ru>
2020-04-22 17:36:05 +0300

    lease enter the commit message for your changes. Lines starting

--- assets/js/logo.js
+++ assets/js/logo.js
@@ -1,72 +1,60 @@
-const scene = new THREE.Scene();
-let fieldOfView = 75,
-    aspectRatio = window.innerWidth/window.innerHeight,
+"use strict";
+
+var scene = new THREE.Scene();
+var fieldOfView = 75,
+    aspectRatio = window.innerWidth / window.innerHeight,
     near = 0.1,
     far = 400;
-const camera = new THREE.PerspectiveCamera( fieldOfView, aspectRatio, near, far );
+var camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
 camera.position.z = 300;
-
-const light = new THREE.DirectionalLight( 0xffffff );
-light.position.set( 1, 0, 1 ).normalize();
-scene.add(light);
-
-// jupiter
+var light = new THREE.DirectionalLight(0xffffff);
+light.position.set(1, 0, 1).normalize();
+scene.add(light); // jupiter
 // ---------
-let jupiterRadius = 140,
+
+var jupiterRadius = 140,
     jupiterWidthSegments = 50,
     jupiterHeightSegments = 50;
-
-let jupiterMaterial = new THREE.MeshPhongMaterial( {} );
-let loader = new THREE.ImageLoader();
-
-loader.load( "https://raw.githubusercontent.com/afonsopacifer/cdn/master/jupitermap.jpg", ( image ) => {
-  let texture = new THREE.Texture();
+var jupiterMaterial = new THREE.MeshPhongMaterial({});
+var loader = new THREE.ImageLoader();
+loader.load("https://raw.githubusercontent.com/afonsopacifer/cdn/master/jupitermap.jpg", function (image) {
+  var texture = new THREE.Texture();
   texture.image = image;
-  texture.needsUpdate = true;  
+  texture.needsUpdate = true;
   jupiterMaterial.map = texture;
   jupiterMaterial.needsUpdate = true;
 });
-
-const jupiterGeometry = new THREE.SphereGeometry( jupiterRadius, jupiterWidthSegments, jupiterHeightSegments );
-const jupiter = new THREE.Mesh( jupiterGeometry, jupiterMaterial );
-// earth
+var jupiterGeometry = new THREE.SphereGeometry(jupiterRadius, jupiterWidthSegments, jupiterHeightSegments);
+var jupiter = new THREE.Mesh(jupiterGeometry, jupiterMaterial); // earth
 // ---------
-let earthRadius = 12,
+
+var earthRadius = 12,
     earthWidthSegments = 10,
-    earthHeightSegments =10;
-
-let earthMaterial = new THREE.MeshPhongMaterial( {} );
-let loader2 = new THREE.ImageLoader();
-
-loader2.load( "https://raw.githubusercontent.com/afonsopacifer/cdn/master/earthmap1k.jpg", ( image ) => {
-  let texture = new THREE.Texture();
+    earthHeightSegments = 10;
+var earthMaterial = new THREE.MeshPhongMaterial({});
+var loader2 = new THREE.ImageLoader();
+loader2.load("https://raw.githubusercontent.com/afonsopacifer/cdn/master/earthmap1k.jpg", function (image) {
+  var texture = new THREE.Texture();
   texture.image = image;
   texture.needsUpdate = true;
-  
   earthMaterial.map = texture;
   earthMaterial.needsUpdate = true;
 });
+var earthGeometry = new THREE.SphereGeometry(earthRadius, earthWidthSegments, earthHeightSegments);
+var earth = new THREE.Mesh(earthGeometry, earthMaterial);
+scene.add(jupiter, earth);
+var renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const earthGeometry = new THREE.SphereGeometry( earthRadius, earthWidthSegments, earthHeightSegments );
-
-const earth = new THREE.Mesh( earthGeometry, earthMaterial );
-
-scene.add( jupiter, earth );
-
-const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const render = () => {
-  
-    earth.position.set( -230, 0, 0 );
-
-  requestAnimationFrame( render );
+var render = function render() {
+  earth.position.set(-230, 0, 0);
+  requestAnimationFrame(render);
   jupiter.rotation.y += 0.002;
   earth.rotation.y += 0.025;
-  
-  renderer.render( scene, camera );
-  
+  renderer.render(scene, camera);
 };
 
 render();
